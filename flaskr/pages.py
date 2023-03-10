25fe6dd83ee5955001a73bb58b4c12bd3abb8bd3
sajan_acharya <sajan_acharya@techexchange.in>
2023-03-10 08:45:27 +0000

    completed the upload route and added comments

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -20,9 +20,10 @@ def make_endpoints(app):
     def allowed_file(filename):
         return '.' in filename and \
              filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
+    
     @app.route('/upload', methods=['GET', 'POST'])
     def upload_file():
+        """checks the extension and uploads valid files to the content bucket"""
         if request.method == 'POST':
             if 'file' not in request.files:
                 flash('No file part')
