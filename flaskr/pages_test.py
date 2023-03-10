9050574c8b3ee64131fe8087b260f2de9dde84c2
sajan_acharya <sajan_acharya@techexchange.in>
2023-03-10 08:44:55 +0000

    just created the test_upload_route

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -23,3 +23,11 @@ def test_home_page(client):
     assert b"Hello, World!\n" in resp.data
 
 # TODO(Project 1): Write tests for other routes.
+def test_upload_route(client):
+    #need to add the test for this part
+    pass
+
+
+
+
+
